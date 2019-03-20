9f31e2948c754c47a0c6ef6d47f5c08c94a9255a
sannonthachai <sannonthachai@gmail.com>
2019-03-20 17:36:20 +0700

    commit

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,6 @@
 $('body').AutoProvince({
 		PROVICE:		'#province', // select div สำหรับรายชื่อจังหวัด
-		AMPHUR:			'#sub-province', // select div สำหรับรายชื่ออำเภอ
+		AMPHUR:			'#amphur', // select div สำหรับรายชื่ออำเภอ
 		DISTRICT:		'#district', // select div สำหรับรายชื่อตำบล
 		POSTCODE:		'#postcode', // input field สำหรับรายชื่อรหัสไปรษณีย์
 		arrangeByName:		false // กำหนดให้เรียงตามตัวอักษร
