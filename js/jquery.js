b425489a5cd3e4b9b518041d307e7c7c274a3af1
sannonthachai <sannonthachai@gmail.com>
2019-03-20 16:01:09 +0700

    commit

---
+++ js/jquery.js
@@ -0,0 +1,7 @@
+$('body').AutoProvince({
+		PROVICE:		'#province', // select div สำหรับรายชื่อจังหวัด
+		AMPHUR:			'#sub-province', // select div สำหรับรายชื่ออำเภอ
+		DISTRICT:		'#district', // select div สำหรับรายชื่อตำบล
+		POSTCODE:		'#postcode', // input field สำหรับรายชื่อรหัสไปรษณีย์
+		arrangeByName:		false // กำหนดให้เรียงตามตัวอักษร
+	});
